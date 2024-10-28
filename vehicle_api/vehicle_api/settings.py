0b8257dcf4e90f033f4cf3f12ee3ef264f78b6c1
Brenden Price <price.brenden01@gmail.com>
2024-10-28 18:55:17 -0400

    Update DEBUG env var to be null safe

--- vehicle_api/vehicle_api/settings.py
+++ vehicle_api/vehicle_api/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('VAPI_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if bool(os.environ['DEBUG']):
+if bool(os.environ.get('DEBUG', False)):
     DEBUG = True
 
 ALLOWED_HOSTS = ['*']
